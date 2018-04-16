b410eb35f923d67958abfa350a30f354c6d0db54
samia <samia.ahmed91@gmail.com>
2018-04-15 18:50:55 -0700

    updating API call to add area specific call

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -8,18 +8,31 @@ import { InterlinkService } from './../interlink.service';
 })
 export class HomeComponent implements OnInit {
   myData: Array<any>;
+  userArea: object 
 
   constructor(private _interlinkService: InterlinkService) {
     this.myData = []
+    this.userArea = {
+      area:""
+    }
   }
 
   find_data() {
-    console.log("hey gurl")
+    // console.log("hey gurl")
     this._interlinkService.find_data((res) => {
       this.myData.push(res)
-      console.log(this.myData)
+      // console.log(this.myData)
     })
   }
+
+  getArea() {
+    // console.log("hi im getting the area")
+    this._interlinkService.getArea(this.userArea,(res) => {
+      this.userArea = res
+      console.log("here is your area object:", this.userArea)
+    })
+  }
+
   ngOnInit() {
 
   }
--- src/app/interlink.service.ts
+++ src/app/interlink.service.ts
@@ -3,18 +3,31 @@ import { Http } from '@angular/http';
 
 @Injectable()
 export class InterlinkService {
-  
-  constructor(private _http: Http) { 
+  area = "";
 
+  constructor(private _http: Http) { 
+    this.area = ""
   }
 
   find_data(cb){
-    console.log("hi from service")
+    // console.log("hi from service")
     this._http.get('http://data.seattle.gov/resource/fdax-a9ur.json?study_year=2017&study_area=Ballard&study_time=12:00 PM').subscribe((res)=>{
         cb(res.json())
       })
   }
-  firstCall(v,cb){
-    this._http.get('http://data.seattle.gov/resource/fdax-a9ur.json?study_year=2017&study_area=${{v}}&study_time=12:00 PM')
+
+  getArea(area, cb){
+    // console.log("hi getting area from service")
+    console.log("area:", area.area,"string:",'http://data.seattle.gov/resource/fdax-a9ur.json?study_year=2017&study_area='+area.area+'&study_time=12:00 PM')
+    
+    this._http.get('http://data.seattle.gov/resource/fdax-a9ur.json?study_year=2017&study_area=}' + area.area+ '&study_time=12:00 PM').subscribe((res)=>{
+      console.log("area on the way back:", res.json())
+        this.area = area.area
+        cb(res.json())
+      })
   }
+
+  // firstCall(v,cb){
+  //   this._http.get('http://data.seattle.gov/resource/fdax-a9ur.json?study_year=2017&study_area=${{v}}&study_time=12:00 PM')
+  // }
 }
